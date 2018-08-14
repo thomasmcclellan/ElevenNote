4492260419b7bd3a298925aa43b36fe9181617bd
Thomas McClellan <thomasmcclellan87@gmail.com>
2018-08-14 11:32:23 -0400

    9-10

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Web.Mvc;
+using ElevenNote.Models;
 using ElevenNote.Services;
 using ElevenNote.WebMVC.Models;
 using Microsoft.AspNet.Identity;
@@ -51,7 +52,67 @@ namespace ElevenNote.WebMVC.Controllers
 
             return View(model);
         }
-      
+
+        public ActionResult Edit(int id)
+        {
+            var service = CreateNoteService();
+            var detail = service.GetNoteById(id);
+            var model =
+                new NoteEdit
+                {
+                    NoteId = detail.NoteId,
+                    Title = detail.Title,
+                    Content = detail.Content
+                };
+            return View(model);
+        }
+
+        [ActionName("Delete")]
+        public ActionResult Delete(int id)
+        {
+            var svc = CreateNoteService();
+            var model = svc.GetNoteById(id);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletePost(int id)
+        {
+            var service = CreateNoteService();
+
+            service.DeleteNote(id);
+
+            TempData["SaveResult"] = "Your note was deleted";
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, NoteEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            if (model.NoteId != id)
+            {
+                ModelState.AddModelError("", "Id Mismatch");
+                return View(model);
+            }
+
+            var service = CreateNoteService();
+
+            if (service.UpdateNote(model))
+            {
+                TempData["SaveResult"] = "Your note was updated.";
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Your note could not be updated.");
+            return View();
+        }
 
         private NoteService CreateNoteService()
         {
@@ -60,4 +121,4 @@ namespace ElevenNote.WebMVC.Controllers
             return service;
         }
     }
-}
+}   
